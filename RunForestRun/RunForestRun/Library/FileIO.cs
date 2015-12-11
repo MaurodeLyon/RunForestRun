e8af626160e866240f3ed9b2bb78d2f7082cb7f0
Hanburgah <arthur.brink1@gmail.com>
2015-12-11 21:04:30 +0100

    FileIO (opt moment Strings, kan mogelijk ook met JSON)

---
+++ RunForestRun/RunForestRun/Library/FileIO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Windows.Storage;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RunForestRun.Library
+{
+    class FileIO
+    {
+        public static async void Save(String filename, String contents)
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync(filename+ ".data", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+
+            await Windows.Storage.FileIO.WriteTextAsync(sampleFile, contents);
+
+        }
+
+        public static async Task<string> Load(String filename)
+        {
+
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("sample.txt");
+
+            String contents = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+
+            return contents;
+
+
+        }
+
+    }
+}
