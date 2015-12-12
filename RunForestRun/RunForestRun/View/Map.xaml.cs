9ce5937abdade735731639f7937e1f10fb1b9806
Hanburgah <arthur.brink1@gmail.com>
2015-12-12 18:43:42 +0100

    added MVVM structure + Location button /Permission

--- RunForestRun/RunForestRun/View/Map.xaml.cs
+++ RunForestRun/RunForestRun/View/Map.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +23,58 @@ namespace RunForestRun.View
     /// </summary>
     public sealed partial class Map : Page
     {
+        private Geolocator geolocator;
+
         public Map()
         {
             this.InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (geolocator == null)
+            {
+                geolocator = new Geolocator
+                {
+                    DesiredAccuracy = PositionAccuracy.High,
+                    MovementThreshold = 1
+                };
+                //geolocator.PositionChanged += GeolocatorPositionChanged;
+                //GeofenceMonitor.Current.GeofenceStateChanged += GeofenceStateChanged;
+                
+                Geoposition d = await geolocator.GetGeopositionAsync();
+
+                var pos = new Geopoint(d.Coordinate.Point.Position);
+
+
+                await map.TrySetViewAsync(pos,50);
+            }
+            else
+            {
+                //GeofenceMonitor.Current.GeofenceStateChanged -= GeofenceStateChanged;
+                //geolocator.PositionChanged -= GeolocatorPositionChanged;
+                Geoposition d = await geolocator.GetGeopositionAsync();
+
+                var pos = new Geopoint(d.Coordinate.Point.Position);
+
+
+                await map.TrySetViewAsync(pos,50);
+                geolocator = null;
+            }
+        }
+
+        //private void GeolocatorPositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        //{
+            
+        //    var pos = new Geopoint(new BasicGeoposition
+        //    { Latitude = position.Latitude, Longitude = position.Longitude });
+
+        //    DrawCarIcon(pos);
+
+
+        //    await MyMap.TrySetViewAsync(pos, MyMap.ZoomLevel, …);
+        //}
+
     }
 }
+
